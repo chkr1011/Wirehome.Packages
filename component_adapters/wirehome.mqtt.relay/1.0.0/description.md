861862c4580e5bf5809cb9f610e0f4894d823bec
Christian Kratky <christian.kratky@googlemail.com>
2018-10-13 22:01:29 +0200

    [component_adapter/wirehome.mqtt.relay] Add support for backward channel.

--- component_adapters/wirehome.mqtt.relay/1.0.0/description.md
+++ component_adapters/wirehome.mqtt.relay/1.0.0/description.md
@@ -1,5 +1,4 @@
 # Summary
-
 This adapter publishes a MQTT messages if the state is being changed. The used topic and payload for the states `on` and `off` can be modified.
 
 # Configuration
@@ -10,12 +9,24 @@ Example:
 {
     "config": {
         "on": {
+            "server": "192.168.1.10", // Optional broker address
             "topic": "bed/$patch/lamp-left",
             "payload": "on"
         },
         "off": {
+            "server": "192.168.1.10", // Optional broker address
             "topic": "bed/$patch/lamp-left",
             "payload": "off"
+        },
+        "backward_channel": {
+            "on": {
+                "topic": "bed/$status/lamp-left",
+                "payload": "on"
+            },
+            "off": {
+                "topic": "bed/$status/lamp-left",
+                "payload": "on"
+            }
         }
     }
 }
--- component_adapters/wirehome.mqtt.relay/1.0.0/script.py
+++ component_adapters/wirehome.mqtt.relay/1.0.0/script.py
@@ -1,5 +1,5 @@
-def process_adapter_message(properties):
-    type = properties.get("type", None)
+def process_adapter_message(message):
+    type = message.get("type", None)
 
     if type == "initialize":
         return __initialize__()
@@ -15,22 +15,76 @@ def process_adapter_message(properties):
 
 
 def __initialize__():
+    backward_channel = config.get("backward_channel", None)
+    if backward_channel == None:
+        return
+
+    component_uid = scope["component_uid"]
+    on_topic = backward_channel["on"]["topic"]
+    subscription_uid = "wirehome.mqtt.relay.backward_channel.on:" + component_uid
+    mqtt.subscribe(subscription_uid, on_topic, __handle_backward_channel_message__)
+
+    off_topic = backward_channel["off"]["topic"]
+    if off_topic != on_topic:
+        subscription_uid = "wirehome.mqtt.relay.backward_channel.off:" + component_uid
+        mqtt.subscribe(subscription_uid, off_topic, __handle_backward_channel_message__)
+
     return {"type": "success"}
-    
+
 
 def __set_state__(state):
     if state == "on":
-        topic = config["on"]["topic"]
-        payload = config["on"].get("payload", "on")
+        message = config.get("on", {})
     else:
-        topic = config["off"]["topic"]
-        payload = config["off"].get("payload", "off")
+        message = config.get("off", {})
 
     parameters = {
-        "topic": topic,
-        "payload": payload
+        "topic": message.get("topic", None),
+        "payload": message.get("payload", None)
     }
 
-    mqtt.publish(parameters)
+    server = message.get("server", None)
+    if server != None:
+        parameters["server"] = server
+        mqtt.publish_external(parameters)
+    else:
+        mqtt.publish(parameters)
 
-    return {"type": "success"}
+    result = {
+        "type": "success"
+    }
+
+    if config.get("update_status_from_backward_channel_only", False) == True:
+        result["skip_status_update"] = True
+
+    return result
+
+
+def __handle_backward_channel_message__(message):
+    backward_channel = config.get("backward_channel", None)
+    if backward_channel == None:
+        return
+
+    if __is_match__(message, backward_channel.get("on")):
+        publish_adapter_message({
+            "type": "power_state_changed",
+            "power_state": "on"
+        })
+    elif __is_match__(message, backward_channel.get("off")):
+        publish_adapter_message({
+            "type": "power_state_changed",
+            "power_state": "off"
+        })
+
+
+def __is_match__(message, filter):
+    filter_topic = filter.get("topic")
+    topic = message.get("topic")
+
+    if topic != filter_topic:
+        return False
+
+    payload = convert.to_string(message.get("payload", ""))
+    filter_payload = convert.to_string(filter.get("payload", ""))
+
+    return payload == filter_payload
