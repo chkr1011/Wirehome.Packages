f9fe00c165e4e22952d04eac55ccb408728d086c
Christian Kratky <christian.kratky@googlemail.com>
2018-11-11 20:42:47 +0100

    [automation.wirehome.roller_shutter_assistant\1.0.0] Fix breaking changes.

--- automation.wirehome.roller_shutter_assistant/1.0.0/script.py
+++ automation.wirehome.roller_shutter_assistant/1.0.0/script.py
@@ -3,20 +3,21 @@ import time
 
 
 def initialize():
-    # wirehome.debugger.enable()
+    wirehome.debugger.enable()
     pass
 
 
 def activate():
-    global sunrise_last_executed_day, sunset_last_executed_day
-    sunrise_last_executed_day = None
-    sunset_last_executed_day = None
-
-    global timer_uid
-    timer_uid = wirehome.context["automation_uid"] + ".timer"
-    wirehome.scheduler.start_timer(timer_uid, 60000, __timer_tick__)
+    global sunrise_last_executed, sunset_last_executed, hot_close_last_executed
+    sunrise_last_executed = None
+    sunset_last_executed = None
+    hot_close_last_executed = None
 
     __timer_tick__(None)
+
+    global timer_uid
+    timer_uid = context["automation_uid"] + ".timer"
+    wirehome.scheduler.start_timer(timer_uid, 60000, __timer_tick__)
 
 
 def deactivate():
@@ -32,67 +33,48 @@ def get_status():
 def __timer_tick__(_):
     wirehome.debugger.clear_trace()
 
+    global sunrise_last_executed, sunset_last_executed, hot_close_last_executed
+
     today = datetime.datetime.today()
     now = today.time()
 
-    if sunset_last_executed_day != today.day:
-        __invoke_sunset__(now, today.day)
-    elif sunrise_last_executed_day != today.day:
-        __invoke_sunrise__(now, today.day)
+    command = None
+
+    if __is_sunrise_affected__(now) and sunrise_last_executed != today.day:
+        command = "move_up"
+        sunrise_last_executed = today.day
+
+    if __is_sunset_affected__(now) and sunset_last_executed != today.day:
+        command = "move_down"
+        sunset_last_executed = today.day
+
+    if __is_outdoor_temperature_high__() and hot_close_last_executed != today.day:
+        command = "move_down"
+        hot_close_last_executed = today.day
+
+    if command != None:
+        if not __check_outdoor_temperature_is_low_(command):
+            __invoke_roller_shutter__(command)
 
 
-def __invoke_sunset__(now, day):
-    close_on_sunset = wirehome.automation.get_setting("close_on_sunset", True)
-
-    if close_on_sunset == False:
-        return
-
+def __is_sunset_affected__(now):
     sunset_shift = wirehome.automation.get_setting("sunset_shift", "00:00:00")
     sunset = __parse_time__("outdoor.sunset", sunset_shift)
-    sunset_is_affected = now >= sunset
-
-    if not sunset_is_affected:
-        return
-
-    global sunset_last_executed_day
-    sunset_last_executed_day = day
-
-    if not __outdoor_temperature_is_ok__():
-        # TODO: Add method and add more details. (Consider adding details for notifications in general and use resources.)
-        wirehome.notifications.publish("information", "Skipped closing roller shutters because outdoor temperature is low.")
-    else:
-        __invoke_roller_shutter__("move_down")
+    return now >= sunset
 
 
-def __invoke_sunrise__(now, day):
-    open_on_sunrise = wirehome.automation.get_setting("open_on_sunrise", True)
-
-    if open_on_sunrise == False:
-        return
-
+def __is_sunrise_affected__(now):
     disabled_before = wirehome.automation.get_setting("disabled_before", None)
     if disabled_before != None:
         disabled_before = datetime.datetime.strptime(disabled_before, '%H:%M').time()
 
-        if disabled_before > now:
+        if now < disabled_before:
             wirehome.debugger.trace("disabled before is affected")
-            return
+            return False
 
     sunrise_shift = wirehome.automation.get_setting("sunrise_shift", "00:00:00")
     sunrise = __parse_time__("outdoor.sunrise", sunrise_shift)
-    sunrise_is_affected = now >= sunrise
-
-    if not sunrise_is_affected:
-        return
-
-    global sunrise_last_executed_day
-    sunrise_last_executed_day = day
-
-    if not __outdoor_temperature_is_ok__():
-        # TODO: Add method and add more details. (Consider adding details for notifications in general and use resources.)
-        wirehome.notifications.publish("information", "Skipped opening roller shutters because outdoor temperature is low.")
-    else:
-        __invoke_roller_shutter__("move_up")
+    return now >= sunrise
 
 
 def __invoke_roller_shutter__(command):
@@ -120,14 +102,40 @@ def __parse_time__(global_variable_name, shift):
     return value.time()
 
 
-def __outdoor_temperature_is_ok__():
-    min_outdoor_temperature = wirehome.automation.get_setting("min_outdoor_temperature", None)
-    if min_outdoor_temperature == None:
-        return True
+def __check_outdoor_temperature_is_low_(command):
+    min_temperature = wirehome.automation.get_setting("min_outdoor_temperature", None)
+    if min_temperature == None:
+        return False
 
-    outdoor_temperature = wirehome.global_variables.get("outdoor_temperature", None)
+    outdoor_temperature = wirehome.global_variables.get("outdoor.temperature", None)
     if outdoor_temperature == None:
-        return True
+        return False
 
-    outdoor_temperature = float(outdoor_temperature)
-    return outdoor_temperature >= min_outdoor_temperature
+    is_low = float(outdoor_temperature) < float(min_temperature)
+
+    if not is_low:
+        return False
+
+    message = ""
+
+    # TODO: Use resource service for translation
+    if command == "move_down":
+        message = "Skipped closing roller shutters because outdoor temperature is low ({outdoor_temperature})."
+    elif command == "move_up":
+        message = "Skipped opening roller shutters because outdoor temperature is low ({outdoor_temperature})."
+
+    wirehome.notifications.publish("information", message.format(outdoor_temperature=outdoor_temperature))
+
+    return True
+
+
+def __is_outdoor_temperature_high__():
+    max_temperature = wirehome.automation.get_setting("auto_close_outdoor_temperature", None)
+    if max_temperature == None:
+        return False
+
+    outdoor_temperature = wirehome.global_variables.get("outdoor.temperature", None)
+    if outdoor_temperature == None:
+        return False
+
+    return float(max_temperature) > float(outdoor_temperature)
