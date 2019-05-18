14b56638246dc69bdfec8ebe6a2df537a2debc90
Christian Kratky <christian.kratky@googlemail.com>
2019-05-18 20:52:06 +0200

    Add MQTT multi sensor.

---
+++ wirehome.device_adapters.mqtt.multi_sensor/1.0.0/description.md
@@ -0,0 +1,20 @@
+# Summary
+This adapters maps several MQTT topics of a sensor and pushes them to the component logic. The value of the sensor is read from the payload. The value of the payload will be converted to a string in all cases.
+Each individual value from an MQTT topic is mapped via a custom key.
+
+# Configuration
+
+Example:
+
+```json
+{
+    "config": {
+        "key1": {
+            "topic": "mySensorTopic/0"
+        },
+        "key2": {
+            "topic": "mySensorTopic/1"
+        }
+    }
+}
+```
---
+++ wirehome.device_adapters.mqtt.multi_sensor/1.0.0/script.py
@@ -0,0 +1,35 @@
+config = {}
+
+
+def process_adapter_message(message):
+    type = message.get("type", None)
+
+    if type == "initialize":
+        return __initialize__()
+
+    return wirehome.response_creator.not_supported(type)
+
+
+def __initialize__():
+    for key in config:
+        subscription_uid = "wirehome.mqtt.multi_sensor/" + wirehome.context["component_uid"] + "/" + key
+
+        subscription = config.get(key)
+        topic = subscription["topic"]
+
+        def f(m, k=key): __handle_mqtt_message__(k, m)
+        wirehome.mqtt.subscribe(subscription_uid, topic, f)
+
+
+def __handle_mqtt_message__(key, mqtt_message):
+    payload = mqtt_message["payload"]
+    payload_string = wirehome.convert.to_string(payload)
+
+    adapter_message = {
+        "type": "value_received",
+        "key": key,
+        "value": payload_string,
+        "value_type": "string"
+    }
+
+    wirehome.publish_adapter_message(adapter_message)
