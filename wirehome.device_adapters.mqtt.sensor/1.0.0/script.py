47b88c282186d08333b05e70607acd74a09396ef
Christian Kratky <christian.kratky@googlemail.com>
2019-05-18 20:52:42 +0200

    Refactor MQTT sensor.

--- wirehome.device_adapters.mqtt.sensor/1.0.0/script.py
+++ wirehome.device_adapters.mqtt.sensor/1.0.0/script.py
@@ -1,32 +1,29 @@
 config = {}
 
 
-def process_adapter_message(properties):
-    type = properties.get("type", None)
+def process_adapter_message(message):
+    type = message.get("type", None)
 
     if type == "initialize":
         return __initialize__()
 
-    return {
-        "type": "exception.not_supported",
-        "origin_type": type
-    }
+    return wirehome.response_creator.not_supported(type)
 
 
 def __initialize__():
-    subscription_uid = "wirehome.mqtt.sensor:" + wirehome.context["component_uid"]
+    subscription_uid = "wirehome.mqtt.sensor/" + wirehome.context["component_uid"]
     topic = config["topic"]
     wirehome.mqtt.subscribe(subscription_uid, topic, __handle_mqtt_message__)
 
 
-def __handle_mqtt_message__(properties):
-    payload = properties["payload"]
+def __handle_mqtt_message__(mqtt_message):
+    payload = mqtt_message["payload"]
     payload_string = wirehome.convert.to_string(payload)
 
-    properties = {
+    adapter_message = {
         "type": "value_updated",
         "value": payload_string,
         "value_type": "string"
     }
 
-    wirehome.publish_adapter_message(properties)
+    wirehome.publish_adapter_message(adapter_message)
