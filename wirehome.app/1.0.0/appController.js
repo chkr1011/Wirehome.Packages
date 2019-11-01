b4ee86ba165319a7eab78e66e3bf0c8252377a21
Christian Kratky <christian.kratky@googlemail.com>
2019-11-01 14:25:36 +0100

    Improve app performance by only updating when state has changed.

--- wirehome.app/1.0.0/appController.js
+++ wirehome.app/1.0.0/appController.js
@@ -8,7 +8,7 @@ function createAppController($http, $scope, apiService, localizationService, com
     c.panels = [];
     c.componentGroups = [];
     c.globalVariables = {};
-    
+
     c.notificationService = notificationService;
     c.componentService = componentService;
     c.macroService = macroService;
@@ -66,59 +66,55 @@ function createAppController($http, $scope, apiService, localizationService, com
                 if (componentGroup.settings["app.is_visible"] === false) {
                     return;
                 }
-                
+
                 var componentGroupModel = {
                     uid: componentGroup.uid,
+                    source: {},
+                    settings: {},
                     components: [],
-                    macros: []
-                };
-
-                componentGroupModel.getSetting = function (uid, defaultValue) {
-                    return getValue(componentGroupModel.source.settings, uid, defaultValue);
-                };
-
-                componentGroupModel.hasSetting = function (uid) {
-                    return componentGroupModel.source.settings.hasOwnProperty(uid);
+                    macros: [],
+                    hasSetting: function (uid) {
+                        return componentGroupModel.settings.hasOwnProperty(uid);
+                    },
+                    getSetting: function (uid, defaultValue) {
+                        return getValue(componentGroupModel.settings, uid, defaultValue);
+                    }
                 };
 
                 $.each(componentGroup.components, function (i) {
                     var componentModel = {
                         uid: i,
                         source: {},
-                        showMore: false
-                    };
-
-                    componentModel.toggleShowMore = function() {
-                        componentModel.showMore = !componentModel.showMore;
-                    };
-
-                    componentModel.hasStatus = function (uid) {
-                        return componentModel.source.status.hasOwnProperty(uid);
-                    };
-
-                    componentModel.getStatus = function (uid, defaultValue) {
-                        return getValue(componentModel.source.status, uid, defaultValue);
-                    };
-
-                    componentModel.hasSetting = function (uid) {
-                        var associationSettings = componentGroupModel.source.components[componentModel.uid].settings;
-                        return associationSettings.hasOwnProperty(uid) ||
-                            componentModel.source.settings.hasOwnProperty(uid);
-                    };
-
-                    componentModel.getSetting = function (uid, defaultValue) {
-                        var associationSettings = componentGroupModel.source.components[componentModel.uid].settings;
-                        return getEffectiveValue([associationSettings, componentModel.source.settings],
-                            uid,
-                            defaultValue);
-                    };
-
-                    componentModel.hasConfiguration = function (uid) {
-                        return componentModel.source.configuration.hasOwnProperty(uid);
-                    };
-
-                    componentModel.getConfiguration = function (uid, defaultValue) {
-                        return getValue(componentModel.source.configuration, uid, defaultValue);
+                        status: {},
+                        settings: {},
+                        configuration: {},
+                        showMore: false,
+                        toggleShowMore: function () {
+                            componentModel.showMore = !componentModel.showMore;
+                        },
+                        hasStatus: function (uid) {
+                            return componentModel.status.hasOwnProperty(uid);
+                        },
+                        getStatus: function (uid, defaultValue) {
+                            return getValue(componentModel.status, uid, defaultValue);
+                        },
+                        hasSetting: function (uid) {
+                            var associationSettings = componentGroupModel.source.components[componentModel.uid].settings;
+                            return associationSettings.hasOwnProperty(uid) ||
+                                componentModel.settings.hasOwnProperty(uid);
+                        },
+                        getSetting: function (uid, defaultValue) {
+                            var associationSettings = componentGroupModel.source.components[componentModel.uid].settings;
+                            return getEffectiveValue([associationSettings, componentModel.settings],
+                                uid,
+                                defaultValue);
+                        },
+                        hasConfiguration: function (uid) {
+                            return componentModel.source.configuration.hasOwnProperty(uid);
+                        },
+                        getConfiguration: function (uid, defaultValue) {
+                            return getValue(componentModel.source.configuration, uid, defaultValue);
+                        }
                     };
 
                     componentGroupModel.components.push(componentModel);
@@ -127,18 +123,16 @@ function createAppController($http, $scope, apiService, localizationService, com
                 $.each(componentGroup.macros, function (i) {
                     var macroModel = {
                         uid: i,
-                        source: {}
-                    };
-
-                    macroModel.hasSetting = function (uid) {
-                        var associationSettings = macroModel.source.macros[macroModel.uid].settings;
-                        return associationSettings.hasOwnProperty(uid) ||
-                            macroModel.source.settings.hasOwnProperty(uid);
-                    };
-
-                    macroModel.getSetting = function (uid, defaultValue) {
-                        var associationSettings = componentGroupModel.source.macros[macroModel.uid].settings;
-                        return getEffectiveValue([associationSettings, macroModel.source.settings], uid, defaultValue);
+                        source: {},
+                        hasSetting: function (uid) {
+                            var associationSettings = macroModel.source.macros[macroModel.uid].settings;
+                            return associationSettings.hasOwnProperty(uid) ||
+                                macroModel.source.settings.hasOwnProperty(uid);
+                        },
+                        getSetting: function (uid, defaultValue) {
+                            var associationSettings = componentGroupModel.source.macros[macroModel.uid].settings;
+                            return getEffectiveValue([associationSettings, macroModel.source.settings], uid, defaultValue);
+                        }
                     };
 
                     componentGroupModel.macros.push(macroModel);
@@ -150,6 +144,13 @@ function createAppController($http, $scope, apiService, localizationService, com
             if (c.componentGroups.length === 1) {
                 c.setActivePanel(c.componentGroups[0].uid);
             }
+
+            c.panels = status.panels;
+            c.panels.push({
+                uid: "componentGroups",
+                positionIndex: 0,
+                viewSource: "views/componentGroupsPanelTemplate.html"
+            });
 
             c.isConfigured = true;
         }
@@ -166,13 +167,8 @@ function createAppController($http, $scope, apiService, localizationService, com
         });
 
         c.notifications = status.notifications;
-        c.globalVariables = status.global_variables;
-        c.panels = status.panels;
-        c.panels.push({
-            uid: "componentGroups",
-            positionIndex: 0,
-            viewSource: "views/componentGroupsPanelTemplate.html"
-        });
+
+        importChanges(c.globalVariables, status.global_variables);
 
         c.isInitialized = true;
     };
@@ -189,26 +185,16 @@ function createAppController($http, $scope, apiService, localizationService, com
         model.sortValue = getEffectiveValue([associationSettings, source.settings], "app.position_index", model.uid);
     };
 
-    c.configureComponent = function (model, source, componentGroupModel) {
-        model.source = source;
-
-        model.viewSource = getValue(source.configuration, "app.view_source", null);
-        if (model.viewSource === undefined || model.viewSource === null) {
-            model.viewSource = "views/viewMissingTemplateView.html";
-        }
-
-        var associationSettings = componentGroupModel.source.components[model.uid].settings;
-        model.sortValue = getEffectiveValue([associationSettings, source.settings], "app.position_index", model.uid);
-    };
-
     c.configureComponentGroup = function (model, source, status) {
         model.source = source;
 
-        model.sortValue = getValue(source.settings, "app.position_index", model.uid);
+        importChanges(model.settings, source.settings);
+
+        model.sortValue = getValue(model.settings, "app.position_index", model.uid);
 
         // Sort the components by its sort value. Do not use sorting in Angular because the
         // actual is required for other code.
-        model.components.sort(function(x, y) { return x.sortValue - y.sortValue; });
+        model.components.sort(function (x, y) { return x.sortValue - y.sortValue; });
 
         $.each(model.components,
             function (i, componentModel) {
@@ -221,35 +207,49 @@ function createAppController($http, $scope, apiService, localizationService, com
             });
     };
 
-    c.moveComponent = function(component, componentGroup, direction){
+    c.configureComponent = function (model, source, componentGroupModel) {
+        model.source = source;
+
+        importChanges(model.settings, source.settings);
+        importChanges(model.status, source.status);
+        importChanges(model.configuration, source.configuration);
+
+        if (model.viewSource === undefined || model.viewSource === null) {
+            model.viewSource = getValue(model.configuration, "app.view_source", null);
+            if (model.viewSource === undefined || model.viewSource === null) {
+                model.viewSource = "views/viewMissingTemplateView.html";
+            }
+        }
+
+        var associationSettings = componentGroupModel.source.components[model.uid].settings;
+        model.sortValue = getEffectiveValue([associationSettings, model.settings], "app.position_index", model.uid);
+    };
+
+    c.moveComponent = function (component, componentGroup, direction) {
 
         var itemIndex = componentGroup.components.indexOf(component);
 
-        if (direction == "down")
-        {
-            if (itemIndex == componentGroup.components.length - 1)
-            {
+        if (direction == "down") {
+            if (itemIndex == componentGroup.components.length - 1) {
                 return;
             }
 
             [componentGroup.components[itemIndex], componentGroup.components[itemIndex + 1]] = [componentGroup.components[itemIndex + 1], componentGroup.components[itemIndex]];
         }
-        else if (direction == "up")
-        {
-            if (itemIndex == 0)
-            {
+        else if (direction == "up") {
+            if (itemIndex == 0) {
                 return;
             }
 
             [componentGroup.components[itemIndex], componentGroup.components[itemIndex - 1]] = [componentGroup.components[itemIndex - 1], componentGroup.components[itemIndex]];
         }
-        
+
         $.each(componentGroup.components,
             function (i, component) {
                 c.componentService.setPositionIndex(component.uid, componentGroup.uid, i);
             });
     };
-    
+
     $http.get("version.txt").then(function (response) {
         c.version = response.data;
     });
@@ -287,4 +287,29 @@ function getValue(source, name, defaultValue) {
     }
 
     return source[name];
+}
+
+function importChanges(target, source) {
+    $.each(source, function (key) {
+        var newValue = source[key];
+
+        if (!target.hasOwnProperty(key)) {
+            target[key] = newValue;
+            return;
+        }
+
+        var oldValue = target[key];
+
+        if (angular.toJson(newValue) === angular.toJson(oldValue)) {
+            return;
+        }
+
+        if (newValue === oldValue) {
+            return;
+        }
+
+        target[key] = newValue;
+
+        console.log("Updated '" + key + "' from '" + oldValue + "' to '" + newValue + "'.")
+    });
 }
